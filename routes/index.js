577e118d0b573b31ca710f5e16df62e59fa8618d
Christopher Wray <chris@sol.company>
2023-08-27 14:38:04 -0700

    add messages and new

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Messages", messages: messages });
+});
+
+router.get("/new", function (req, res, next) {
+  res.render("form", { title: "New Message" });
+});
+
+router.post("/new", function (req, res, next) {
+  const { user, text } = req.body;
+  messages.push({
+    text: text,
+    user: user,
+    added: new Date(),
+  });
+  res.redirect("/");
 });
 
 module.exports = router;
